b31e546cff8daf1990736ad3db7a17465ce8fa46
Nutthasit Chuaychoocherd <68010378@kmitl.ac.th>
2026-01-16 10:24:18 +0700

    p01_7

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,14 @@
-export function getEdgePosts() {}
+import { getEdgePosts } from './p01';
+
+(async () => {
+  try {
+    console.log("Fetching data...");
+    
+    const result = await getEdgePosts();
+
+    console.log("Result:", JSON.stringify(result, null, 2));
+    
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
