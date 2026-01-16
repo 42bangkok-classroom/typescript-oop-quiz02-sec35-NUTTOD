22544eef389c23fe24e4929b2ca9f51d2f1df3b8
Nutthasit Chuaychoocherd <68010378@kmitl.ac.th>
2026-01-16 10:36:38 +0700

    p02_1

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,30 @@
-export function getPostsByUser() {}
+import axios from 'axios';
+
+interface ApiPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface EdgePost {
+  id: number;
+  title: string;
+}
+
+export const getEdgePosts = async (id: number): Promise<EdgePost[]> => {
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const { data } = await axios.get<ApiPost[]>(url);
+
+    return data
+      .filter((post) => post.id === id)
+      .map(({id, title}) => ({
+        id,
+        title
+      }));
+
+  } catch (error) {
+    throw error;
+  }
+};
